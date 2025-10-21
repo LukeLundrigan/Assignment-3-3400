846f9d88760cfebcf9d7cf27db801759f7050f81
LukeLundrigan <lglundrigan@mun.ca>
2025-10-21 18:54:56 -0230

    Add files via upload

---
+++ 3400assignment3.cpp
@@ -0,0 +1,126 @@
+#include "Rational.hpp"
+#include <stdexcept>
+using namespace std;
+
+Rational::Rational(Integer numerator, Integer denominator)
+	: numerator(numerator), denominator(denominator)
+{
+	if (denominator == 0)
+		throw invalid_argument("Denominator cannot be zero");
+}
+
+Rational::Rational(Rational&& other)
+	:numerator(other.numerator), denominator(other.denominator)
+{
+	other.numerator = 0;
+	other.denominator = 1;
+}
+
+Rational::Rational(const Rational& other)
+	:numerator(other.numerator), denominator(other.denominator)
+{
+}
+
+Rational& Rational::operator=(const Rational& other)
+{
+	if (this != &other)
+	{
+		numerator = other.numerator;
+		denominator = other.denominator;
+	}
+	return *this;
+}
+
+Rational& Rational::operator=(const Rational&& other)
+{
+	if (this != &other)
+	{
+		numerator = other.numerator;
+		denominator = other.denominator;
+	}
+	return *this;
+}
+
+Rational Rational::operator+(const Rational& other)
+{
+	return Rational(
+		numerator * other.denominator + other.numerator * denominator,
+		denominator * other.denominator
+	);
+}
+
+Rational Rational::operator-(const Rational& other)
+{
+	return Rational(
+		numerator * other.denominator - other.numerator * denominator,
+		denominator * other.denominator
+	);
+}
+
+Rational Rational::operator*(const Rational& other)
+{
+	return Rational(
+		numerator * other.numerator,
+		denominator * other.denominator
+	);
+}
+
+Rational Rational::operator/(const Rational& other)
+{
+	if (other.numerator == 0)
+		throw invalid_argument("Cannot divide by zero");
+	
+	return Rational(
+		numerator * other.denominator,
+		denominator * other.numerator
+	);
+}
+
+Rational& Rational::operator+=(const Rational& other)
+{
+	numerator = numerator * other.denominator + other.numerator * denominator;
+	denominator *= other.denominator;
+	return *this;
+}
+
+Rational& Rational::operator-=(const Rational& other)
+{
+	numerator = numerator * other.denominator - other.numerator * denominator;
+	denominator *= other.denominator;
+	return *this;
+}
+
+Rational& Rational::operator*=(const Rational& other)
+{
+	numerator *= other.numerator;
+	denominator *= other.denominator;
+	return *this;
+}
+
+Rational& Rational::operator/=(const Rational& other)
+{
+	if (other.numerator == 0)
+		throw invalid_argument("Denominator cannot be zero");
+
+	numerator *= other.denominator;
+	denominator *= other.numerator;
+	return *this;
+}
+
+double Rational::toDouble()
+{
+	return static_cast<double>(numerator) / denominator;
+}
+
+ostream& operator<<(ostream& out, const Rational& Rational)
+{
+	return Rational.print(out) const;
+}
+
+istream& operator>>(istream& in, Rational& Rational)
+{
+	return Rational.scan(in);
+}
+
+// Keeping the rational in lowest terms would be good as it is cleaner to look at and understand from users POV
+// It also can help with run times if the fractions are really big numbers that could be reduced
